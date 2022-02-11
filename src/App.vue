d6fb14ad1ad22e65bd93cf3d3e9520df1b06e919
Warodom <warodom@mm.co.th>
2022-02-12 01:16:54 +0700

    add app bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="transparent" absolute dense class="elevation-0">
+      <v-app-bar-title class="text-uppercase" style="color: #e50815">
+        Champignon
+      </v-app-bar-title>
+      <div v-if="$vuetify.breakpoint.smAndUp" class="d-flex">
+        <div
+          v-for="(menu, index) in menuBar"
+          class="ml-2"
+          style="font-size: 14px"
+          :key="index"
+        >
+          {{ menu.title }}
+        </div>
+      </div>
+      <div v-else>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="transparent"
+              class="text-capitalize elevation-0"
+              v-bind="attrs"
+              v-on="on"
+              style="font-size: 12px"
+            >
+              Browse
+              <v-icon x-small>mdi-triangle mdi-rotate-180</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(menu, index) in menuBar" :key="index">
+              <v-list-item-title>{{ menu.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+      <v-spacer></v-spacer>
+      <v-icon class="mr-4" small @click="doSearch()">mdi-magnify</v-icon>
+      <v-icon class="mr-4" small @click="doSearch()">mdi-bell</v-icon>
+      <v-avatar class="rounded" tile color="yellow" size="35">
+        <v-icon color="black"> mdi-account </v-icon>
+      </v-avatar>
+    </v-app-bar>
+
+    <v-main> </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    // HelloWorld,
+  },
+
+  data: () => ({
+    menuBar: [
+      { title: "Home" },
+      { title: "TV Shows" },
+      { title: "Movies" },
+      { title: "New & Popular" },
+      { title: "My List" },
+    ],
+  }),
+  methods: {
+    doSearch() {
+      console.log("search");
+    },
+  },
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/netflix-layout/'
-    : '/'
+    : '/',
+
+    transpileDependencies: [
+      'vuetify'
+    ]
 }
