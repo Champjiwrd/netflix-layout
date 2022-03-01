3ba9ad426590c2dca87594728c423df29b322bc4
Warodom <warodom@mm.co.th>
2022-03-02 01:57:50 +0700

    deploy

---
+++ js/All artist.28a3763b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["All artist"],{"2d0d":function(n,t,e){"use strict";e.r(t);var a=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("v-container",[n._v("ศิลปินทั้งหมด")])},l=[],r={},c=r,i=e("2877"),o=e("6544"),s=e.n(o),u=e("a523"),p=Object(i["a"])(c,a,l,!1,null,null,null);t["default"]=p.exports;s()(p,{VContainer:u["a"]})}}]);
+//# sourceMappingURL=All artist.28a3763b.js.map
